7dd04b2f5c888996f94a45f0c3fb509a95f55c79
Alex Leybourne <alexleybourne1@gmail.com>
2020-01-04 05:59:02 +1000

    Log in completed

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const User = require('../model/User')
 const bcrypt = require('bcryptjs')
-const {registerValidation} = require('../validation')
+const {registerValidation, loginValidation} = require('../validation')
 
 router.post('/register', async (req,res) => {
 
@@ -25,11 +25,29 @@ router.post('/register', async (req,res) => {
     })
     try{ 
         const savedUser = await user.save()
-        res.send(savedUser)
+        res.send({user: user._id})
     } catch(err){
         res.status(400).send(err)
     }
 })
 
+// Login
+router.post('/login',async  (req, res) => {
+    // Validate new user data with Joi
+    const {error} = loginValidation(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
 
+    // Checks if email exists
+    const user = await User.findOne({email: req.body.email})
+    if(!user) return res.status(400).send('Email or Password is incorrect')
+
+    // Check Password
+    const validPass = await bcrypt.compare(req.body.password,  user.password)
+    if (!validPass) return res.status(400).send('Email or Password is incorrect')
+
+    res.send('Logged In!')
+
+})
+
+  
 module.exports = router
