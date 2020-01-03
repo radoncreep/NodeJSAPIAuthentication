a00e9774d68eb1068fe2178136c5e04cfd2b8e69
Alex Leybourne <alexleybourne1@gmail.com>
2020-01-03 22:38:48 +1000

    Email Duplicate Check on signup

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,19 @@ const {registerValidation} = require('../validation')
 
 router.post('/register', async (req,res) => {
 
-    // Validate with Joi
+    // Validate new user data with Joi
     const {error} = registerValidation(req.body)
     if(error) return res.status(400).send(error.details[0].message)
 
+    // Checks if user already exists
+    const emailExists = await User.findOne({email: req.body.email})
+    if(emailExists) return res.status(400).send('This Email is already Registered')
+
+    // Creates new user
     const user = new User({
          name: req.body.name,
          email: req.body.email,
-         password: req.body.password
+         password: req.body.password 
     })
     try{
         const savedUser = await user.save()
