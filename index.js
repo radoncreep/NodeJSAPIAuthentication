77b5dd03c3bbdfbadd50349a3a35cba57562d978
Alex Leybourne <alexleybourne1@gmail.com>
2020-01-03 15:46:57 +1000

    Routes and request

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+// Import Routes
+const authRoute = require('./routes/auth')
+
+
+
+// Route Middlewares
+
+app.use('/api/user', authRoute)
+
+app.listen(3000, () => console.log('Server is up and Running!'))
