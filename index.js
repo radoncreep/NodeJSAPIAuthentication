1b5d9ac27d9b8081e46f3979e660bbb99fe201fe
Alex Leybourne <alexleybourne1@gmail.com>
2020-01-03 16:32:10 +1000

    Connected to DB

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+dotenv.config()
+
+// Connect to Database
+mongoose.connect(
+process.env.DB_CONNECT, 
+{ useNewUrlParser: true },
+() => console.log('Connected to DB') 
+)
 
 // Import Routes
 const authRoute = require('./routes/auth')
