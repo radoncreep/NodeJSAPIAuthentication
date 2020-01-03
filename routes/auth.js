6eea5b42f5f72383e1711929ee06ce27748ee36d
Alex Leybourne <alexleybourne1@gmail.com>
2020-01-04 06:21:09 +1000

    Tokens Added

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const User = require('../model/User')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const {registerValidation, loginValidation} = require('../validation')
 
 router.post('/register', async (req,res) => {
@@ -45,7 +46,9 @@ router.post('/login',async  (req, res) => {
     const validPass = await bcrypt.compare(req.body.password,  user.password)
     if (!validPass) return res.status(400).send('Email or Password is incorrect')
 
-    res.send('Logged In!')
+    // Create and Assign Token
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET)
+    res.header('auth-token', token).send(token)
 
 })
 
