9caca87a2d4fb9005404dc296bf0c4e998787a8c
Alex Leybourne <alexleybourne1@gmail.com>
2020-01-03 17:16:27 +1000

    Can post users

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 
+// Import Routes
+const authRoute = require('./routes/auth')
+
 dotenv.config()
 
 // Connect to Database
@@ -12,13 +15,11 @@ process.env.DB_CONNECT,
 () => console.log('Connected to DB') 
 )
 
-// Import Routes
-const authRoute = require('./routes/auth')
 
-
+// Middleware
+app.use(express.json())
 
 // Route Middlewares
-
 app.use('/api/user', authRoute)
 
 app.listen(3000, () => console.log('Server is up and Running!'))
