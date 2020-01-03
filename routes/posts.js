2742daecae76a387cbaf45e8023d9d00b50b20ed
Alex Leybourne <alexleybourne1@gmail.com>
2020-01-04 06:34:12 +1000

    Auth Token Checked

---
+++ routes/posts.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const verify = require('./verifyToken') 
+
+router.get('/', verify, (req,res) => {
+    res.json({
+        posts: {
+            title: 'my first post', 
+            description: 'wassup my homie' }
+    })
+})
+
+module.exports = router
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,12 +1,13 @@
-const jwt = require('jsonwebtoken') 
+const jwt = require('jsonwebtoken')
 
-function auth (req,res,next){
+module.exports = function (req,res,next){
     const token = req.header('auth-token')
     if (!token) res.status(401).send('Access Denied')
 
     try {
         const verified = jwt.verify(token, process.env.TOKEN_SECRET)
         req.user = verified
+        next()
     } catch(error) {
         res.status(400).send('Invalid Token')
     }
